564c3ee40d78f0e8165a4d45920d41f48e888cc1
mtrenkina <rita-trenkina1997@yandex.ru>
2022-11-22 16:15:21 +0300

    import is working

--- gulpfile.js
+++ gulpfile.js
@@ -46,6 +46,7 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/less/*.less", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
 };
 
